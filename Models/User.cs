455bc2cf1043a3c6b6625ea3c02a6045a9d4161a
DIDI <DIDI@LAPTOP-1FDIP285>
2024-11-25 00:47:49 +0800

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int UID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength (100)]
+        public string PhoneNumber { get; set; }
+    }
+}
